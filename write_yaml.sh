f5aba7636cb21f894715ba03922f866c600e6865
Rina Takase <rina.m.takase@gmail.com>
2016-10-27 13:58:02 +0900

    add write_yaml.sh

---
+++ write_yaml.sh
@@ -0,0 +1,12 @@
+#!usr/bin/env bash
+
+yaml=emojis.yaml
+
+echo "title: devicon" > $yaml
+echo "emojis:" >> $yaml
+
+for png in png/*.png
+do
+  echo "  - name: $png" >> $yaml
+  echo "   src: https://raw.githubusercontent.com/takose/slack_stamp_adder/master/png/$png" >> $yaml
+done
